245c1e5bd575decea25851956c19343c1adb84f1
Nakildias <130524745+Nakildias@users.noreply.github.com>
2025-03-26 01:27:01 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ if [ -z "$(command -v python3)" ]; then
 
   # If apt is available (Debian/Ubuntu)
   if command -v apt &> /dev/null; then
-    sudo apt update && sudo apt install python3 python3-pip
+    sudo apt update && sudo apt install python3 python3-pip python3.12-venv
   fi
 
   # If dnf is available (Fedora)
